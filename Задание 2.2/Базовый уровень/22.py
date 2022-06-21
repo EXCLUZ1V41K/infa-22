8b9ceabaec978fb5c22e200390257dd48ec7602f
Александр <105676290+EXCLUZ1V41K@users.noreply.github.com>
2022-06-21 21:01:23 +0300

    Create 22.py

---
+++ Задание 2.2/Базовый уровень/22.py
@@ -0,0 +1,12 @@
+v1 = float(input("V1 = "))
+m1 = float(input("m1 = "))
+v2 = float(input("V2 = "))
+m2 = float(input("m2 = "))
+p1 = v1 * m1
+p2 = v2 * m2
+if p1 > p2:
+    print("Плотность первого материала больше")
+elif p1 < p2:
+    print("Плотность второго материала больше")
+else:
+    print("Ои равны")
